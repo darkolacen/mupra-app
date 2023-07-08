9e8b4d0aa72b781cbcca337596276cc29e0780a5
dusanlacen <97316662+dusanlacen@users.noreply.github.com>
2023-07-08 12:09:59 +0200

    Update MainSection.jsx

--- client/src/views/ContactPage/Sections/MainSection.jsx
+++ client/src/views/ContactPage/Sections/MainSection.jsx
@@ -45,7 +45,7 @@ class MainSection extends React.Component {
                   <h4 className={classes.cardSubtitle}><small>ID za DDV:</small> SI86230905</h4>
                   <h4 className={classes.cardSubtitle}><small>Matična številka:</small> 6412807</h4>
                   <h4 className={classes.cardSubtitle}><small>TRR:</small> SI56 0510 0801 3699 496</h4>
-                  <h4 className={classes.cardSubtitle}><small>Odprt pri:</small> ABANKA VIPA d.d.</h4>
+                  <h4 className={classes.cardSubtitle}><small>Odprt pri:</small> ABANKA VIPA D.D</h4>
                 </CardBody>
               </Card>
             </GridItem>
